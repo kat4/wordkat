d05724013257659b7b83ab74a7ffd1bcfef3f9e6
katkelemen <kelemenkata@gmail.com>
2015-10-01 16:15:27 +0100

    trying to

---
+++ autocomplete.js
@@ -0,0 +1,20 @@
+var acjs = (function() {
+
+  var filter_word = function(word_content, word_array) {
+      return word_array.filter(function(word) {
+          if (word.slice(0, word_content.length) == word_content) {
+            return true;
+          }
+      });
+  };
+
+  return {
+    filter_word: filter_word
+
+  };
+
+
+  //when add additional letter added return a request using the previous 3 letters and a fourth
+}());
+
+module.exports = acjs;
--- js/ac.js
+++ js/ac.js
@@ -1,26 +1,22 @@
 var acjs = (function() {
   //get value from searchbar
   search_value = document.getElementById("search-box").value;
-  search_array = search_value.split("");
+  //search_array = search_value.split("");
 
-  // if key press and key press content > == 3 then call filter_word
+  function defRequest() {
 
-  //get first 3 letters of string typed in box and send request, put that into array.
-  var filter_word = function(word_content, word_array) {
-      return word_array.filter(function(word) {
-          if (word.slice(0, word_content.length) == word_content) {
-            return true;
-          }
-      });
-  };
+    var xhttp = new XMLHttpRequest();
 
-  return {
-    filter_word: filter_word
+    xhttp.onreadystatechange = function() {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+        var result = xhttp.responseText;
+      }
+    };
+    xhttp.open("GET", "/search/"+search_value , true);
+    xhttp.send();
 
-  };
+  }
 
+  defRequest();
 
-  //when add additional letter added return a request using the previous 3 letters and a fourth
 }());
-
-//module.exports = acjs;
