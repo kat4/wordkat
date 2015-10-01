db0215d71a4c8fe5fbef54326d5cd034a5c9568e
mantagen <mathsgregory@gmail.com>
2015-10-01 21:56:32 +0100

    wip

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ var wordkat = (function() {
 
 
     var server = http.createServer(handler);
-    //server.listen(process.env.PORT || PORT);
+    server.listen(process.env.PORT || PORT);
 
     return {
         handler: handler
