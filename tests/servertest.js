1166efe4086b8c7bb5da8167d7d43ea3ea7a9bc4
katkelemen <kelemenkata@gmail.com>
2015-10-02 11:50:44 +0100

    closes #24

--- tests/servertest.js
+++ tests/servertest.js
@@ -2,65 +2,79 @@ var test = require('tape');
 var Shot = require('shot');
 var wordkat = require('../server.js');
 
-test("check 1 is equal to 1", function (t) {
-    t.equal(1, 1, "success!");
-    t.end();
+test("check 1 is equal to 1", function(t) {
+  t.equal(1, 1, "success!");
+  t.end();
 });
-
-test('return the string "success"', function (t) {
-	Shot.inject(wordkat.handler, {method: 'GET', url: "/success"}, function (res) {
-		t.equal(res.payload, 'success', "Congrats");
-	});
+test('return the string "success"', function(t) {
+  Shot.inject(wordkat.handler, {
+    method: 'GET',
+    url: "/success"
+  }, function(res) {
+    t.equal(res.payload, 'success', "Congrats");
     t.end();
+  });
 });
-test('return the string "success"', function (t) {
-	Shot.inject(wordkat.handler, {method: 'GET', url: "/worddef/cat"}, function (res) {
-		t.equal(!!res.payload, true, "Congrats");
-	});
+test('return the string "success"', function(t) {
+  Shot.inject(wordkat.handler, {
+    method: 'GET',
+    url: "/worddef/cat"
+  }, function(res) {
+    t.equal(!!res.payload, true, "Congrats");
     t.end();
+  });
 });
-test('return the string "success"', function (t) {
-	Shot.inject(wordkat.handler, {method: 'GET', url: "/js/ac.js"}, function (res) {
-		t.equal(!!res.payload, true, "Congrats");
-	});
+test('return the string "success"', function(t) {
+  Shot.inject(wordkat.handler, {
+    method: 'GET',
+    url: "/js/ac.js"
+  }, function(res) {
+    t.equal(!!res.payload, true, "Congrats");
     t.end();
+  });
 });
-test('return the string "success"', function (t) {
-	Shot.inject(wordkat.handler, {method: 'GET', url: "/js/wn.js"}, function (res) {
-		t.equal(!!res.payload, true, "Congrats");
-	});
+test('return the string "success"', function(t) {
+  Shot.inject(wordkat.handler, {
+    method: 'GET',
+    url: "/js/wn.js"
+  }, function(res) {
+    t.equal(!!res.payload, true, "Congrats");
     t.end();
+  });
 });
-test('return the string "success"', function (t) {
-	Shot.inject(wordkat.handler, {method: 'GET', url: "/wordnik.js"}, function (res) {
-		t.equal(!!res.payload, true, "Congrats");
-	});
+test('return the string "success"', function(t) {
+  Shot.inject(wordkat.handler, {
+    method: 'GET',
+    url: "/wordnik.js"
+  }, function(res) {
+    t.equal(!!res.payload, true, "Congrats");
     t.end();
+  });
 });
-test('return the string "success"', function (t) {
-	Shot.inject(wordkat.handler, {method: 'GET', url: "/autocomplete.js"}, function (res) {
-		t.equal(!!res.payload, true, "Congrats");
-	});
+test('return the string "success"', function(t) {
+  Shot.inject(wordkat.handler, {
+    method: 'GET',
+    url: "/autocomplete.js"
+  }, function(res) {
+    t.equal(!!res.payload, true, "Congrats");
     t.end();
+  });
 });
-test('return the string "success"', function (t) {
-	Shot.inject(wordkat.handler, {method: 'GET', url: "/frontendtests.js"}, function (res) {
-		t.equal(!!res.payload, true, "Congrats");
-	});
+test('return the string "success"', function(t) {
+  Shot.inject(wordkat.handler, {
+    method: 'GET',
+    url: "/frontendtests.js"
+  }, function(res) {
+    t.equal(!!res.payload, true, "Congrats");
     t.end();
+  });
 });
-test('filter array of words from response by searchterm', function (t) {
-	Shot.inject(wordkat.handler, {method: 'GET', url: "/search/cat"}, function (res) {
-		t.equal(!!res.payload, true, "Congrats");
-	});
+test('filter array of words from response by searchterm', function(t) {
+  Shot.inject(wordkat.handler, {
+    method: 'GET',
+    url: "/search/cat"
+  }, function(res) {
+    t.equal(!!res.payload, true, "Congrats");
     t.end();
+  });
 });
-
-
-// test('keypress after 2 letters in inputfield triggers a request', function (t) {
-//   var textfieldValue =
-// 	Shot.inject(wordkat.handler, {method: 'GET', url: "/search/cat"}, function (res) {
-//
-// 		t.end();
-// 	});
-// });
