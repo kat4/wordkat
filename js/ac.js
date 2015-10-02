cd29e80289bc011407cf969e75f957446245fc4a
katkelemen <kelemenkata@gmail.com>
2015-10-02 11:41:23 +0100

    closes #26 missing semicolons

--- js/ac.js
+++ js/ac.js
@@ -1,66 +1,24 @@
 var acjs = (function() {
-    var searchField = document.getElementById('search-box');
-    var activeWordIndex = 0;
+  //get value from searchbar
+  search_value = document.getElementById("search-box").value;
+  //search_array = search_value.split("");
 
-    function acRequest(acTerm, callback) {
+  
 
-        var xhttp = new XMLHttpRequest();
+  function defRequest() {
 
-        xhttp.onreadystatechange = function() {
-            if (xhttp.readyState == 4 && xhttp.status == 200) {
-                callback(JSON.parse(xhttp.responseText));
-            }
-        };
-        xhttp.open("GET", "/search/" + acTerm, true);
-        xhttp.send();
+    var xhttp = new XMLHttpRequest();
 
-    }
+    xhttp.onreadystatechange = function() {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+        var result = xhttp.responseText;
+      }
+    };
+    xhttp.open("GET", "/search/"+search_value , true);
+    xhttp.send();
 
-    function acCallback(array) {
-        var listContainer = document.getElementById('autocomplete-suggestions');
-        console.log(array);
-        var growingList = "";
-        var thisWord;
-        for (var i = 0; i < array.length; i++) {
-            thisWord = array[i];
-            growingList += '<li class="suggestion">' + thisWord + '</li>';
-        }
-        listContainer.innerHTML = growingList;
-        listContainer.firstChild.classList.add('active');
-        activeWordIndex = 0;
-    }
+  }
 
-    searchField.addEventListener('keydown', function(e) {
-        if (e.keyCode === 38 || e.keyCode === 40) {
-            e.preventDefault();
-        }
-    });
-    searchField.addEventListener('keyup', function(e) {
-        var suggestionList = document.getElementsByClassName('suggestion');
-        if (e.keyCode === 38 || e.keyCode === 40) {
-            if (e.keyCode === 40 && activeWordIndex < suggestionList.length-1) {
-                activeWordIndex++;
-            }
-            if (e.keyCode === 38 && activeWordIndex > 0) {
-                activeWordIndex--;
-            }
-            for (var i = 0; i < suggestionList.length; i++) {
-                if (i !== activeWordIndex) {
-                    suggestionList[i].classList.remove("active");
-                } else {
-                    suggestionList[i].classList.add("active");
-                }
-            }
-            activeWord = document.getElementsByClassName('active')[0];
-            if (activeWord) {
-                apiTrigger.defRequest(activeWord.textContent, function(data) {
-                    document.getElementById('definition-container').innerHTML = '<p>' + data + '</p>';
-                });
-            }
-
-        } else if (searchField.value.length > 2) {
-            acRequest(searchField.value, acCallback);
-        }
-    });
+  defRequest();
 
 }());
--- wordnik.js
+++ wordnik.js
@@ -13,13 +13,13 @@ function wnRequest(searchTerm, callback) {
     var wordnikurl = "http://api.wordnik.com:80/v4/word.json/" + searchTerm + "/definitions?limit=200&includeRelated=true&useCanonical=false&includeTags=false&api_key=83916c7411db7255406860c49b9083f65a950aff27e3c6a5d";
     request.get(wordnikurl, function(error, response, body) {
         if (error) {
-            return
+            return;
         }
         if (response.statusCode !== 200) {
-            return
+            return;
         } else {
             var object = JSON.parse(body);
-            
+
             if (!object[0]) {
                 callback('Sorry, this word is way too obscure for kat4... Search for "cat" instead.');
 
@@ -32,17 +32,4 @@ function wnRequest(searchTerm, callback) {
 
 }
 
-// function ourCallback(data) {
-//   console.log(data);
-// }
-//
-
-
-// test("check we are getting a response from the API", function(t) {
-//   wnRequest("cat", function(data) {
-//     t.equal(!!data, true, "success!");
-//     t.end();
-//   });
-//
-// });
 module.exports = wnRequest;
