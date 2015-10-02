50e95b00c79a310a1202c710747c62f63ec1fde5
katkelemen <kelemenkata@gmail.com>
2015-10-02 13:08:30 +0100

    delete unnecessary comments closes #17

--- tests/frontendtests.js
+++ tests/frontendtests.js
@@ -1,22 +1,13 @@
 setTimeout(function() {
 		var iframe_select = document.getElementById('iframe');
 		var iframe_content = iframe_select.contentDocument || iframe_select.contentWindow.document;
+		var frontScript = iframe_select.contentWindow.autocomplete;
 
 	test('having an h1 tag', function(assert){
 		var heading = iframe_content.querySelector('h1');
 		assert.equal(!!heading, true);
 
 	});
-
-	var frontScript = iframe_select.contentWindow.autocomplete;
-
-	// test('keypress after 2 letters in inputfield triggers a request', function(assert){
-	// 	var searchTerm = iframe_content.getElementById('search-box').value;
-	// 	console.log(searchTerm);
-	// 	var e = jQuery.Event("keypress");
-	// 	$("#theInputToTest").trigger(e);
-	// 	assert.equal(!!searchTerm, true);
-	// });
 
 	test("check we are getting a response from our nodejs server", function(assert) {
 		var done = assert.async();
